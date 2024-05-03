62190caee5bbd023b600b89479f30cdb67f1dcda
NgKhacDuy <2002nguyenkhacduy@gmail.com>
2024-05-03 14:35:30 +0700

    add style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,9 @@
-import { Link, Outlet } from "react-router-dom";
 function Header() {
   return (
     <>
       <header className="flex justify-between items-center p-5 bg-[#282c34]">
-        <nav>
-          <ul className="list-none inline">
-            <li className="inline mx-[10px] text-white">
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link className="inline mx-[10px] text-white" to="/about">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link className="inline mx-[10px] text-white" to="/portfolio">
-                Portfolio
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        
       </header>
-      <div id="detail">
-        <Outlet />
-      </div>
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,14 @@ import SimpleSlider from "./Slider";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-black text-2xl">Introduce about Jet Fighter</h1>
-      <p className="text-sm mb-4 mt-4">
+    <div className="flex flex-col items-center justify-center bg-[#282c34]">
+      <header className="w-full h-1/5 py-4 bg-orange-400"></header>
+
+      <h1 className="text-white text-2xl">Introduce about Jet Fighter</h1>
+      <p className="text-white text-sm mb-4 mt-4">
         Jet fighter is a game about fighting between jets
       </p>
-      <p className="text-sm mb-4 mt-4">
+      <p className="text-white text-sm mb-4 mt-4">
         Read installation via{" "}
         <a
           href="https://github.com/NgKhacDuy/ma-nguon-mo"
@@ -18,7 +20,6 @@ export default function Home() {
         </a>
       </p>
       <SimpleSlider />
-
       <MemberSlider />
     </div>
   );
